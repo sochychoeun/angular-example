19bb850945f70b8263ee29760eedbb2cbbc1300e
Sochy CHOEUN <sochy.choeun@gmail.com>
2015-05-29 10:32:50 +0700

    Todo task app example

--- public_html/get-start-todolist/todo.js
+++ public_html/get-start-todolist/todo.js
@@ -6,4 +6,26 @@ angular.module('todoApp', [])
                 {text: 'Learn AngularJS first todo list', done:true},
                 {text: 'Create example app with angularjs', done:false}
             ];
+            
+            // Count all uncheck checkbox
+            todo.remaining = function(){
+                var count = 0;
+                angular.forEach(todo.todos, function(task){
+                    count += !task.done? 1: 0;
+                });
+                return count;
+            }
+            
+            // Add new task
+            todo.addTask = function(){
+                todo.todos.push({text: todo.taskText, done: false});
+                todo.taskText = '';
+            }
+            
+            // Remove selected tasks
+            todo.removeTasks = function(){
+                todo.todos =  todo.todos.filter(function(task){
+                    return !task.done;
+                });
+            }
         });
